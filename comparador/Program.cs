79fcf8ab95b516af472c8833cf22e1a769cab946
eduardofmonteiro <dudmont1@gmail.com>
2019-10-09 00:11:57 -0300

    master

---
+++ comparador/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace comparador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comparador de inteiros!");
+
+            Console.WriteLine("Insira o primeiro inteiro:");
+            Int32 n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo inteiro");
+            Int32 n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 == n2)
+            {
+                Console.WriteLine("Os numeros sao iguais!");
+            }
+            else
+            {
+                if (n1 > n2)
+                {
+                    Console.WriteLine(n1 + " é maior!");
+                }
+                else
+                {
+                    Console.WriteLine(n2 + " é maior!");
+                }
+            }
+        }
+    }
+}
